625cfbb8ef5977b1dd974394700a1dc7f7551bd0
Taiwo <taiwo.ibidapoobe@gmail.com>
2019-02-21 17:04:03 +0100

    genrating random number with the Math method and performing DOM manipulation

--- how js works/starter/script.js
+++ how js works/starter/script.js
@@ -95,6 +95,43 @@ function third() {
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+console.log(this);
+
+calculateAge(1994);
+function calculateAge(year) {
+    console.log(2016 - year);
+    // Using the this keyword here will log the window object as it is called in a regular function call. 
+    console.log(this);  
+}
+
+// In the below example, calling the this keyword will log the john object as it is a method (function assigned to an object) call
+var john = {
+    name: 'John',
+    birthYear: 1990,
+    calcAge: function() {
+        console.log(2019 - this.birthYear);
+        console.log(this);
+
+        // Even though this functiion is inside a method, it is still a regular function so the this keyword here will point to the global object. 
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+
+john.calcAge();
+
+
+var mike = {
+    name: 'Mike',
+    birthYear: 1964
+}
+
+// What we did here is called method borrowing. So we made use of the calculate age method that was already defined in the john object and used it
+// for mike to also calculate the age. 
+mike.calcAge = john.calcAge;
+mike.calcAge();
 
 
 
